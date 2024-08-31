fc97405fb9d27ebb2db9837707293ba013e22ba8
Mesut Buğra UYSAL <121828825+bugra123uysal@users.noreply.github.com>
2024-08-31 17:16:19 +0300

    Update kahve_vrs.py
    new graphic added

--- kahve_vrs.py
+++ kahve_vrs.py
@@ -9,10 +9,32 @@ print(data.head())
 print(data.columns)
 clen=data.dropna()
 odeme=data["cash_type"]
+cafe_name=data["coffee_name"]
+cafe_price=data["money"] 
+credit_cart=data['card']
 adet=range(len(odeme))
 
+""" pay types """
 plt.bar(odeme, adet)
-plt.title('ödeme şekli')
 plt.xlabel("Pay kind")
 plt.ylabel("Value")
+plt.title("pay - types")
+plt.grid(True, alpha=0.2)
 plt.show()
+
+
+""" cafe name , price grafik """
+plt.bar(cafe_name ,cafe_price)
+plt.xlabel("name")
+plt.ylabel("price")
+plt.title("cafe- name, price")
+plt.grid(True, alpha=0.2)
+plt.show()
+
+sns.set_style("whitegrid")
+sns.scatterplot(x=cafe_name , y=cafe_price , hue=odeme , size=cafe_price , style=odeme ,data=data)
+plt.xlabel("name")
+plt.ylabel("price")
+plt.show()
+
+
